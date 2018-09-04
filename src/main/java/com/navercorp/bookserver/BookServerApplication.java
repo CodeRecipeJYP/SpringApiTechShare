678f31c4ae2068e7d7b5af26630c1e1f859974ca
Jaeyoung <jaeyoung@AL01352718.local>
2018-09-04 17:50:09 +0900

    Add swagger document

--- src/main/java/com/navercorp/bookserver/BookServerApplication.java
+++ src/main/java/com/navercorp/bookserver/BookServerApplication.java
@@ -7,8 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.zalando.problem.ProblemModule;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BookServerApplication implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(BookServerApplication.class, args);
@@ -29,5 +36,24 @@ public class BookServerApplication implements WebMvcConfigurer {
         configurer
                 .favorParameter(true)
                 .parameterName("format");
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("books")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("서점 API 서버")
+                .description("서점 서비스에서 제공함")
+                .license("Apache License Version 2.0")
+                .version("2.0")
+                .build();
     }
 }
