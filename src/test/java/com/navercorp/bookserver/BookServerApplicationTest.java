269d9c29e84f6004cb62a36cbaa0e9bf318bc174
Jaeyoung <jaeyoung@AL01352718.local>
2018-09-04 15:29:03 +0900

    Add maven wrapper / Add test

---
+++ src/test/java/com/navercorp/bookserver/BookServerApplicationTest.java
@@ -0,0 +1,17 @@
+package com.navercorp.bookserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookServerApplicationTest {
+    @Test
+    public void sanitationTest() throws Exception {
+    }
+}
