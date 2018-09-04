39f323796dca5d0fa0e289a76f14f975f1ca90b1
Jaeyoung <jaeyoung@AL01352718.local>
2018-09-04 16:33:14 +0900

    Add test script

--- scripts/test_scripts.sh
+++ scripts/test_scripts.sh
@@ -3,3 +3,7 @@
 curl -H "Content-Type: application/json" -X POST -d '{"title":"사피엔스", "author":"유발하라리"}' http://localhost:8080/api/books
 curl -H "Content-Type: application/json" -X PUT -d '{"title":"사피엔스2", "author":"유발하라리"}' http://localhost:8080/api/books/1
 curl -H "Content-Type: application/json" -X DELETE http://localhost:8080/api/books/1
+
+curl -H "Content-Type: application/json" -X POST -d '{"title":"사피엔스", "author":"유발하라리"}' http://localhost:8080/api/books
+curl -X GET http://localhost:8080/api/books/1?format=xml
+curl -X GET http://localhost:8080/api/books/1?format=json
