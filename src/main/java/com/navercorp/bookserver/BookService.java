b1ee1e986da784d46a12bd5228f71e19fc4c3313
Jaeyoung <jaeyoung@AL01352718.local>
2018-09-04 15:37:42 +0900

    Add failure test

--- src/main/java/com/navercorp/bookserver/BookService.java
+++ src/main/java/com/navercorp/bookserver/BookService.java
@@ -2,22 +2,38 @@ package com.navercorp.bookserver;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 @Service
 public class BookService {
+    private ConcurrentMap<Integer, Book> repo = new ConcurrentHashMap<>();
+    private AtomicInteger maxId = new AtomicInteger(0);
 
     public Book findById(Integer id) {
-        return new Book(1, "Java 이렇게 공부하자", "김자바", 300);
+        return repo.get(id);
     }
 
+    public boolean update(Book book) {
+        Book old = repo.put(book.getId(), book);
+        return old != null;
+    }
+
+    public Book create(Book book) {
+        Integer id = maxId.addAndGet(1);
+        book.setId(id);
+        repo.put(id, book);
+        return book;
+    }
+
+    public boolean delete(Integer id) {
+        Book old = repo.remove(id);
+        return old != null;
+    }
     public Collection<Book> findAll() {
-        return Arrays.asList(
-                new Book(1, "네이버 네비 좋아요", "김광근", 300),
-                new Book(2, "HTTP 완벽 이해하기", "김명호", 300)
-        );
+        return repo.values();
     }
 }
-
